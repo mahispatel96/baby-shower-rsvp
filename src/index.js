d6bd678a3c835f14729eff1132febd9e0fce51e2
mahispatel96 <mahispatel96@gmail.com>
2024-06-06 16:06:10 -0500

    fix for production build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import 'semantic-ui-css/semantic.min.css'
 import App from './App'
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
